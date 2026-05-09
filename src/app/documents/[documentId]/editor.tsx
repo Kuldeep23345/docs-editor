f8a492b1d28067eb73ee2595fbbba42f228026dc
Kuldeep <kunalrajput67718@gmail.com>
2026-05-09 15:54:15 +0530

    feat: implement interactive document ruler component with margin controls

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -18,6 +18,7 @@ import {fontSizeExtension} from "@/extensions/font-size"
 
 import useEditorStore from '@/store/use-editor-store';
 import { LineHeightExtension } from '@/extensions/line-height';
+import Ruler from './ruler';
 
 const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -105,6 +106,8 @@ const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0  print:bg-white  print:overflow-visible">
+
+      <Ruler/>
       <div className="min-w-max flex justify-center w-204 py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
