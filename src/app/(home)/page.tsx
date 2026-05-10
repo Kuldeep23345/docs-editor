a5db1d708af8256d6317dbd4dd8467e1b2659f02
Kuldeep <kunalrajput67718@gmail.com>
2026-05-10 14:57:04 +0530

    feat: integrate Convex backend, schema, and client provider into the project

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,20 @@
-
-import { Button } from '@/components/ui/button';
+'use client';
+import { api } from '../../../convex/_generated/api';
 import Navbar from './navbar';
 import TemplateGallery from './template-gallery';
+import { useQuery } from 'convex/react';
 
 const page = () => {
+  const documetns = useQuery(api.document.getDocuments);
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-   
-          <Navbar/>
-      
+        <Navbar />
       </div>
-     <div className="mt-16">
-      <TemplateGallery/>
-     </div>
+      <div className="mt-16">
+        <TemplateGallery />
+        {JSON.stringify(documetns)}
+      </div>
     </div>
   );
 };
