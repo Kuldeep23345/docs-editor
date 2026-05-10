0076d177ddc3a9864e935109635e26bb5633826f
Kuldeep <kunalrajput67718@gmail.com>
2026-05-10 14:34:06 +0530

    feat: implement template gallery with carousel component and add document preview assets

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,19 @@
-import { Suspense } from 'react';
+
 import { Button } from '@/components/ui/button';
 import Navbar from './navbar';
+import TemplateGallery from './template-gallery';
 
 const page = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-        <Suspense fallback={null}>
+   
           <Navbar/>
-        </Suspense>
+      
       </div>
-      <Button variant={'outline'}>Button</Button>
+     <div className="mt-16">
+      <TemplateGallery/>
+     </div>
     </div>
   );
 };
---
+++ src/app/(home)/template-gallery.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { templates } from '@/constants/templates';
+import { cn } from '@/lib/utils';
+
+const TemplateGallery = () => {
+  const isCreating = false;
+  return (
+    <div className="bg-[#F1F3F4]">
+      <div className="max-w-7xl mx-auto px-16 py-6 flex flex-col gap-y-4 ">
+        <h3 className="font-medium">Start a new document</h3>
+        <Carousel>
+          <CarouselContent className="-ml-4">
+            {templates.map((template) => (
+              <CarouselItem
+                key={template.id}
+                className="basis-1/2 sm:basis-1/3 md:basis-1/4 xl:basis-1/6 2xl:basis-1/8 pl-4"
+              >
+                <div
+                  className={cn(
+                    'aspect-3/4 flex flex-col gap-y-2.5',
+                    isCreating && 'pointer-events-none opacity-50'
+                  )}
+                >
+                  <button
+                    disabled={isCreating}
+                    onClick={() => {}}
+                    style={{
+                      backgroundImage: `url(${template.imageUrl})`,
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                    }}
+                    className="size-full hover:border-blue-500 rounded-sm b border hover:bg-blue-50 transition flex flex-col items-center justify-center gap-y-4 bg-white"
+                  />
+
+                  <p className="ml-1 text-sm font-medium truncate">{template.label}</p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default TemplateGallery;
