3ab73194ab2588b5b0b43e48f40354cb8ee96e18
Kuldeep <kunalrajput67718@gmail.com>
2026-05-11 17:30:21 +0530

    feat: integrate organization support into navbar and document operations

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import SearchInput from "./search-input"
-import { UserButton } from "@clerk/nextjs"
+import { UserButton,OrganizationSwitcher } from "@clerk/nextjs"
 
 
 const Navbar = () => {
@@ -14,7 +14,15 @@ const Navbar = () => {
         <h3 className="text-xl">Docs</h3>
       </div>
       <SearchInput/>
+      <div className="flex items-center gap-3 pl-6">
+      <OrganizationSwitcher afterCreateOrganizationUrl="/"
+      afterLeaveOrganizationUrl="/"
+      afterSelectOrganizationUrl="/"
+      afterSelectPersonalUrl="/"
+      
+      />
       <UserButton/>
+      </div>
     </nav>
   )
 }
