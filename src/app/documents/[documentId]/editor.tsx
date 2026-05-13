9729b1fc0745d3eddd798bf1ff207cfe24324a4d
Kuldeep <kunalrajput67718@gmail.com>
2026-05-13 22:26:57 +0530

    feat: enable autofocus on document editor initialization

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -38,6 +38,7 @@ const Editor = ({ initialContent }: EditorProps) => {
   });
   const { setEditor } = useEditorStore();
   const editor = useEditor({
+    autofocus: true,
     onCreate: ({ editor }) => {
       setEditor(editor);
     },
